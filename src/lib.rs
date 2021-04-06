93c63f6bdf16fc974dbbe72ad1638ecefb937944
andrewcushing <andyc.bt4567@gmail.com>
2021-04-06 20:39:59 +0100

    Basically functionality done

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,6 @@ use std::collections::HashMap;
 use std::fs;
 
 pub fn run(opts: HashMap<String, Vec<&String>>, file_path: String) {
-    println!("{:?}", opts);
-
     let delim: String = get_delim(&opts);
     let eol: String = get_eol(&opts);
 
@@ -14,18 +12,57 @@ pub fn run(opts: HashMap<String, Vec<&String>>, file_path: String) {
         .map(|line| line.split(&delim).collect())
         .collect();
 
-    let widths = get_col_widths(data);
+    let widths = get_col_widths(&data);
 
+    let total_width: usize = widths.iter().sum::<usize>() + widths.len() + 1;
 
+    println!("{}", get_table_top(total_width));
 
-    println!("Column widths are: {:?}", widths);
+    for i in 0..data.len() {
+        let mut line: String = String::new();
+        for j in 0..widths.len() {
+            let val = match data[i].get(j) {
+                None => {""}
+                Some(s) => {s}
+            };
+            line.push_str(format!("|{}", fit_to_width(val, widths[j])).as_str());
+        }
+        line.push_str("|");
+        println!("{}", line);
+    }
+
+    println!("{}", get_table_top(total_width));
 }
 
-fn get_col_widths(data: Vec<Vec<&str>>) -> Vec<usize> {
+fn fit_to_width(s: &str, width: usize) -> String {
+    let mut result = String::from(s);
+    for _ in 0..(width - s.len()) {
+        result.push_str(" ");
+    }
+    result
+}
+
+fn get_table_top(width: usize) -> String {
+    let mut result = String::from("-");
+    for i in 1..width {
+        result.push_str("-");
+    }
+    result
+}
+
+fn get_table_end(width: usize) -> String {
+    let mut result = String::from("-");
+    for i in 1..width {
+        result.push_str("-");
+    }
+    result
+}
+
+fn get_col_widths(data: &Vec<Vec<&str>>) -> Vec<usize> {
     let mut widths: Vec<usize> = data[0].iter().map(|s| s.len()).collect();
 
     for line_num in 1..data.len() {
-        for val_num in 0..data[line_num].len() {
+        for val_num in 0..widths.len() {
             if data[line_num][val_num].len() > widths[val_num] {
                 widths.remove(val_num);
                 widths.insert(val_num, data[line_num][val_num].len());
